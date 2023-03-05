aa217d613ccba8a29a17fc623aa7fc4fdf249cb9
BogachevaKA1 <BogachevaKA1@it.mos.ru>
2023-03-05 15:02:43 +0300

    commit for exercise4

--- exercise4/Program.cs
+++ exercise4/Program.cs
@@ -1,26 +1,27 @@
-﻿string Check_N(int N)
+﻿
+void CheckPalindromicNumber(int number)
 {
-    int num1 = N / 1000;
-    int num2 = num1 / 10;
-    int num3 = num1 % 10;
-    int num4 = N % 100;
-    int num5 = num4 / 10;
-    int num6 = num4 % 10;
-    string res = "Введены некоректные данные";
-    if (num2 == num6 & num3 == num5)
-        Console.WriteLine("Число " + N + " является палиндромом");
+    if (number >= 10000 && number < 100000)
+    {
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
+
+        if (division1 == remainder1)
+        {
+            number = number / 10;
+            int division2 = (number / 100) % 10;
+            int remainder2 = number % 10;
+            if (division2 == remainder2)
+                Console.WriteLine("Да");
+        }
+        else
+            Console.WriteLine("Нет");
+
+    }
     else
-        Console.WriteLine("Число " + N + " не является палиндромом");
-    return res;
+        Console.WriteLine("Некорректное число!");
 }
-try
-{
-    Console.WriteLine("Введите пятизначное число для проверки");
-    int n = Convert.ToInt32(Console.ReadLine());
-    Check_N(n);
-    Console.WriteLine(Check_N(n));
-}
-catch
-{
-    Console.WriteLine("Были введены некорректные данные");
-}
+
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+CheckPalindromicNumber(number);
